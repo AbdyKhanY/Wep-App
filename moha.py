8c668a70546621d2703c01b06c19ca7fc193f825
Abdikhani <136441727+AbdyKhanY@users.noreply.github.com>
2024-07-22 20:07:32 +0300

    Update moha.py

--- moha.py
+++ moha.py
@@ -109,12 +109,12 @@ def main():
     inputs['PPE'] = st.text_input('PPE')
 
    # Creating a button for prediction
-        if st.button('Parkinsons Disease Test Result'):
-            try:
-                diagnosis = parkinsons_disease_prediction([MDVP_Fo_Hz, MDVP_Fhi_Hz, MDVP_Flo_Hz, MDVP_Jitter_percent, MDVP_Jitter_Abs, MDVP_RAP, MDVP_PPQ, Jitter_DDP, MDVP_Shimmer, MDVP_Shimmer_dB, Shimmer_APQ3, Shimmer_APQ5, MDVP_APQ, Shimmer_DDA, NHR, HNR, RPDE, DFA, spread1, spread2, D2, PPE])
-                st.success(diagnosis)
-            except ValueError:
-                st.error("Please enter valid numeric values.")
+    if st.button('Parkinsons Disease Test Result'):
+        try:
+            diagnosis = parkinsons_disease_prediction([MDVP_Fo_Hz, MDVP_Fhi_Hz, MDVP_Flo_Hz, MDVP_Jitter_percent, MDVP_Jitter_Abs, MDVP_RAP, MDVP_PPQ, Jitter_DDP, MDVP_Shimmer, MDVP_Shimmer_dB, Shimmer_APQ3, Shimmer_APQ5, MDVP_APQ, Shimmer_DDA, NHR, HNR, RPDE, DFA, spread1, spread2, D2, PPE])
+            st.success(diagnosis)
+        except ValueError:
+            st.error("Please enter valid numeric values.")
 
 if __name__ == '__main__':
     main()
